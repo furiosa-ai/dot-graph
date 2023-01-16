33d69407416f783115ba7e3c9dfc9ec66d1b922c
jaehyun1ee <jaehyun.lee@furiosa.ai>
2023-01-16 15:02:22 +0900

    feat: pretty printer for node

--- src/impls/node.rs
+++ src/impls/node.rs
@@ -1,4 +1,19 @@
+use std::fmt;
 use crate::structs::Node;
 
-impl Node {
+impl fmt::Display for Node {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        fmt.write_str("id = ")?;
+        fmt.write_str(self.id.as_str())?;
+        fmt.write_str("\n")?;
+
+        for (key, value) in &self.attrs {
+            fmt.write_str(key.as_str())?;
+            fmt.write_str(" : ")?;
+            fmt.write_str(value.as_str())?;
+            fmt.write_str("\n")?;
+        }
+
+        Ok(())
+    }
 }
