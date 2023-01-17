041f65e2ee4dd3fcdca27b7b13ab73a4a5fe6fe2
jaehyun1ee <jaehyun.lee@furiosa.ai>
2023-01-17 13:44:34 +0900

    fix: split node attr by \l for prettier print

--- src/impls/node.rs
+++ src/impls/node.rs
@@ -3,15 +3,27 @@ use crate::structs::Node;
 
 impl fmt::Display for Node {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        fmt.write_str("id = ")?;
+        fmt.write_str("id : ")?;
         fmt.write_str(self.id.as_str())?;
-        fmt.write_str("\n")?;
+        fmt.write_str("\n\n")?;
 
         for (key, value) in &self.attrs {
             fmt.write_str(key.as_str())?;
             fmt.write_str(" : ")?;
-            fmt.write_str(value.as_str())?;
-            fmt.write_str("\n")?;
+            let values = value.split("\\l");
+            for (idx, value) in values.enumerate() {
+                if value == "" {
+                    break;
+                }
+
+                // TODO indentation workaround (https://github.com/fdehau/tui-rs/issues/98)
+                if idx > 0 {
+                    let indent = "\u{2800}";
+                    fmt.write_str(format!("{}{}", indent, indent).as_str())?;
+                }
+                fmt.write_str(value)?;
+                fmt.write_str("\n")?;
+            } 
         }
 
         Ok(())
