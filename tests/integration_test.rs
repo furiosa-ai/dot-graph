b34b7ee6611d5c4c96c47f3a16c833494f6e0680
jaehyun1ee-furiosa <jaehyun.lee@furiosa.ai>
2023-02-24 14:10:01 +0900

    refactor: separate tests for each example dots

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,29 +1,61 @@
-use std::fs;
 use std::str;
 
 use dot_graph::prelude::*;
 
+use serial_test::serial;
+
+// all example dot files are from the Graphviz gallery
+// https://graphviz.org/gallery/
+
 #[test]
-fn parse_print_parse() -> Result<(), DotGraphError> {
-    // all example dot files are from Graphviz gallery
-    // https://graphviz.org/gallery/
-    for file in fs::read_dir("./tests/examples").unwrap() {
-        let path = file.unwrap().path();
-        let path = path.to_str().unwrap();
+#[serial]
+fn bazel_build_system() -> Result<(), DotGraphError> {
+    parse_print_parse("bazel_build_system.dot")
+}
 
-        if path.ends_with(".dot") {
-            // first parse from file
-            let graph = parser::parse_from_file(path)?;
+#[test]
+#[serial]
+fn git_basics() -> Result<(), DotGraphError> {
+    parse_print_parse("git_basics.dot")
+}
 
-            // then print to dot
-            let mut dot = Vec::new();
-            graph.to_dot(&mut dot).expect("to_dot should succeed");
-            let dot = str::from_utf8(&dot).unwrap();
+#[test]
+#[serial]
+fn linux_kernel_diagram() -> Result<(), DotGraphError> {
+    parse_print_parse("linux_kernel_diagram.dot")
+}
 
-            // then parse from memory
-            parser::parse_from_memory(dot)?;
-        }
-    }
+#[test]
+#[serial]
+fn neural_network_keras() -> Result<(), DotGraphError> {
+    parse_print_parse("neural_network_keras.dot")
+}
+
+#[test]
+#[serial]
+fn uml_class_diagram_demo() -> Result<(), DotGraphError> {
+    parse_print_parse("uml_class_diagram_demo.dot")
+}
+
+#[test]
+#[serial]
+fn world_dynamics() -> Result<(), DotGraphError> {
+    parse_print_parse("world_dynamics.dot")
+}
+
+fn parse_print_parse(filename: &str) -> Result<(), DotGraphError> {
+    let path = &format!("./tests/examples/{filename}");
+
+    // first parse from file
+    let graph = parser::parse_from_file(path)?;
+
+    // then print to dot
+    let mut dot = Vec::new();
+    graph.to_dot(&mut dot).expect("to_dot should succeed");
+    let dot = str::from_utf8(&dot).unwrap();
+
+    // then parse from memory
+    parser::parse_from_memory(dot)?;
 
     Ok(())
 }
